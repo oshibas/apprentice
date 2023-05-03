f76e0733783480c199d8652844d99178beb5de5e
oshibas <saoritsushima@gmail.com>
2023-05-04 07:32:16 +0900

    commit

--- week3-4/quest/pass.sh
+++ week3-4/quest/pass.sh
@@ -2,12 +2,15 @@
 
 echo "パスワードマネージャーへようこそ！"
 echo "サービス名を入力してください："
+# サービス名をservice_name変数に代入する
 read service_name
 
 echo "ユーザー名を入力してください："
+# ユーザー名をusername変数に代入する
 read username
 
 echo "パスワードを入力してください："
+# -s: 入力されたパスワードを表示させない
 read -s password
 
 # ユーザーの入力をpass.txtに書き込む
