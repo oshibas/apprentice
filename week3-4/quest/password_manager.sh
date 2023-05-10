39e87394d0656f9e4e043213799ca2bb6cc65f27
oshibas <saoritsushima@gmail.com>
2023-05-10 18:18:39 +0900

    oook

--- week3-4/quest/password_manager.sh
+++ week3-4/quest/password_manager.sh
@@ -2,6 +2,7 @@
 # 無限ループ内でユーザーにメニューを表示し、選択肢に応じて適切な処理を行う
 while true; do
     echo "パスワードマネージャーへようこそ！"
+    echo "終了したい場合は Exit と入力してください。"
 
     echo "あなたのGPGキーで設定したメールアドレスを入力してください。"
     # ユーザーからの入力を受け取り、gpg_email変数に代入する。
@@ -10,15 +11,14 @@ while true; do
     gpg_email="${gpg_email:?GPGのメールアドレスを設定してください。}"
 
     echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
-    # ユーザーからの入力を受け取り、choice変数に代入する
     read choice
 
     case $choice in
         "Add Password")
-            # -pオプションでプロンプトを表示
+            # -pオプションでメッセージ（プロンプト）を表示
             # ユーザーからの入力を受け取り、変数に代入する
-            # 変数にデフォルト値が設定されていない場合、エラーメッセージを表示する
             read -p "サービス名を入力してください: " serviceName
+            # 空文字の場合、エラーメッセージを表示する
             serviceName="${serviceName:?サービス名を設定してください。}"
 
             read -p "ユーザー名を入力してください: " userName
@@ -28,35 +28,34 @@ while true; do
             read -s -p "パスワードを入力してください: " password
             password="${password:?パスワードを設定してください。}"
 
-            # デコードしたデータを一時ファイルに保存
+            # 復号化したデータを一時ファイルに保存
+            # 標準エラー出力(2)を/dev/nullに捨てる。つまり、エラーメッセージを表示しない。
             gpg -d password.gpg > password.txt 2> /dev/null
             # パスワードをpassword.txtに書き込む
             echo "$serviceName:$userName:$password" >> passwords.txt
 
            # 入力ファイルを暗号化して出力ファイルに保存する
            gpg -r "$gpg_email" -e -o password.gpg password.txt
-           # 一時ファイルを削除
-           rm password.txt
 
             echo "パスワードの追加は成功しました。"
-            echo "終了したい場合は、 Exit と入力してください。"
             # ;; 各処理の終了を示す
             ;;
         "Get Password")
             read -p "サービス名を入力してください: " serviceName
-            # デコードしたデータを一時ファイルに保存
+            # 復号化したデータを一時ファイルに保存
             gpg -d password.gpg > password.txt 2> /dev/null
             # serviceNameに対応するpassをpasswords.txtファイルから取得
+            # cut -d: -f3: 「:」で区切られた3番目のフィールド（パスワード）を取得
             password=$(grep "^$serviceName:" passwords.txt | cut -d: -f3)
 
             if [ -z "$password" ]; then
                 echo "そのサービスは登録されていません。"
-                echo "終了したい場合は、 Exit と入力してください。"
             else
                 echo "サービス名：$serviceName"
                 # サービス名に対応するユーザー名を表示
+                # cut -d: -f2: 「:」で区切られた2番目のフィールド（ユーザー名）を取得。
                 echo "ユーザー名：$(grep "^$serviceName:" passwords.txt | cut -d: -f2)"
-                # サービス名に対応するパスワードを表示
+                # サービス名に対応するパスワードを表示。
                 echo "パスワード：$password"
             fi
             ;;
@@ -64,9 +63,15 @@ while true; do
             echo "Thank you!"
             exit
             ;;
-        #  *)どの値にも一致しなかった場合の処理
+        # *)どの値にも一致しなかった場合の処理
         *)
             echo "入力が間違えています。Add Password/Get Password/Exitから入力してください。"
             ;;
     esac
 done
+
+    # 一時ファイルを削除する。
+    # tempdir=$(mktemp -d) を使って一時ディレクトリを作成して、それを trap コマンドで削除する。
+    # 実行中に予期せぬエラーが発生した場合でも、一時ファイルを確実に削除できるため。
+    tempdir=$(mktemp -d)
+    trap 'rm -rf "$tempdir"' EXIT
