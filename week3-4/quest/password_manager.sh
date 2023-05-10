3d927152a7bf3d7c240188122905289cfd5fa743
oshibas <saoritsushima@gmail.com>
2023-05-10 19:02:11 +0900

    update

--- week3-4/quest/password_manager.sh
+++ week3-4/quest/password_manager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 無限ループ内でユーザーにメニューを表示し、選択肢に応じて適切な処理を行う
+# 無限ループ内でユーザーにメニューを表示し、選択肢に応じて適切な処理を行う。
 while true; do
     echo "パスワードマネージャーへようこそ！"
     echo "終了したい場合は Exit と入力してください。"
@@ -15,10 +15,10 @@ while true; do
 
     case $choice in
         "Add Password")
-            # -pオプションでメッセージ（プロンプト）を表示
-            # ユーザーからの入力を受け取り、変数に代入する
+            # -pオプションでメッセージ（プロンプト）を表示。
+            # ユーザーからの入力を受け取り、変数に代入する。
             read -p "サービス名を入力してください: " serviceName
-            # 空文字の場合、エラーメッセージを表示する
+            # 空文字の場合、エラーメッセージを表示する。
             serviceName="${serviceName:?サービス名を設定してください。}"
 
             read -p "ユーザー名を入力してください: " userName
@@ -34,25 +34,26 @@ while true; do
             # パスワードをpassword.txtに書き込む
             echo "$serviceName:$userName:$password" >> passwords.txt
 
-           # 入力ファイルを暗号化して出力ファイルに保存する
+           # 入力ファイルを暗号化して出力ファイルに保存する。
            gpg -r "$gpg_email" -e -o password.gpg password.txt
 
             echo "パスワードの追加は成功しました。"
-            # ;; 各処理の終了を示す
+            # ;; 各処理の終了を示す。省略可能。
             ;;
+
         "Get Password")
             read -p "サービス名を入力してください: " serviceName
-            # 復号化したデータを一時ファイルに保存
+            # 復号化したデータを一時ファイルに保存。
             gpg -d password.gpg > password.txt 2> /dev/null
-            # serviceNameに対応するpassをpasswords.txtファイルから取得
-            # cut -d: -f3: 「:」で区切られた3番目のフィールド（パスワード）を取得
+            # serviceNameに対応するpassをpasswords.txtファイルから取得。
+            # cut -d: -f3: 「:」で区切られた3番目のフィールド（パスワード）を取得。
             password=$(grep "^$serviceName:" passwords.txt | cut -d: -f3)
 
             if [ -z "$password" ]; then
                 echo "そのサービスは登録されていません。"
             else
                 echo "サービス名：$serviceName"
-                # サービス名に対応するユーザー名を表示
+                # サービス名に対応するユーザー名を表示。
                 # cut -d: -f2: 「:」で区切られた2番目のフィールド（ユーザー名）を取得。
                 echo "ユーザー名：$(grep "^$serviceName:" passwords.txt | cut -d: -f2)"
                 # サービス名に対応するパスワードを表示。
@@ -63,15 +64,19 @@ while true; do
             echo "Thank you!"
             exit
             ;;
+
         # *)どの値にも一致しなかった場合の処理
         *)
             echo "入力が間違えています。Add Password/Get Password/Exitから入力してください。"
             ;;
+    # case文の終了を示す。esac=caseを逆読みしてる。
     esac
+# whileループの終了を示す。
 done
 
     # 一時ファイルを削除する。
-    # tempdir=$(mktemp -d) を使って一時ディレクトリを作成して、それを trap コマンドで削除する。
+    # tempdir=$(mktemp -d) を使って一時ディレクトリを作成。 trap コマンドで削除する。
     # 実行中に予期せぬエラーが発生した場合でも、一時ファイルを確実に削除できるため。
     tempdir=$(mktemp -d)
+    # trap: 指定したシグナル（ここではEXIT）が発生した場合、コマンド（ここでは'rm -rf "$tempdir"'）を実行させる。
     trap 'rm -rf "$tempdir"' EXIT
