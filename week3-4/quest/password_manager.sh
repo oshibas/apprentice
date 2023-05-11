48631cb427509fd0453c0c3120d3157304429585
oshibas <saoritsushima@gmail.com>
2023-05-11 22:13:55 +0900

    ok

--- week3-4/quest/password_manager.sh
+++ week3-4/quest/password_manager.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# 無限ループ内でユーザーにメニューを表示し、選択肢に応じて適切な処理を行う
+
+PASSWORD_FILE="/home/user/passwords.txt"
+TEMP_PASSWORD_FILE="password.txt"
+ENCRYPTED_PASSWORD_FILE="password.gpg"
+
+# 無限ループ内でユーザーにメニューを表示し、選択肢に応じて適切な処理を行う。
 while true; do
     echo "パスワードマネージャーへようこそ！"
     echo "終了したい場合は Exit と入力してください。"
-
-    echo "あなたのGnuPGキーで設定したメールアドレスを入力してください。"
+    read -p "あなたのGnuPGキーで設定したメールアドレスを入力してください。"gpg_email
     # ユーザーからの入力を受け取り、gnu_email変数に代入する。
-    read gpg_email
     # 「?」演算子: 変数が設定されていない場合にエラーメッセージを表示。
     gpg_email="${gpg_email:?GPGのメールアドレスを設定してください。}"
 
@@ -15,26 +18,26 @@ while true; do
 
     case $choice in
         "Add Password")
-            # -pオプションでメッセージ（プロンプト）を表示
-            # ユーザーからの入力を受け取り、変数に代入する
+            # -pオプションでメッセージ（プロンプト）を表示。
+            # ユーザーからの入力を受け取り、変数に代入する。
             read -p "サービス名を入力してください: " serviceName
-            # 空文字の場合、エラーメッセージを表示する
+            # 空文字の場合、エラーメッセージを表示する。
             serviceName="${serviceName:?サービス名を設定してください。}"
 
             read -p "ユーザー名を入力してください: " userName
             userName="${userName:?ユーザー名を設定してください。}"
 
-            # -s: 入力されたパスワードを表示させない
+            # -s: 入力されたパスワードを表示させない。
             read -s -p "パスワードを入力してください: " password
             password="${password:?パスワードを設定してください。}"
 
-            # 復号化したデータを一時ファイルに保存
+            # 復号化したデータを一時ファイルに保存。
             # 標準エラー出力(2)を/dev/nullに捨てる。つまり、エラーメッセージを表示しない。
             gpg -d password.gpg > password.txt 2> /dev/null
-            # パスワードをpassword.txtに書き込む
+            # パスワードをpassword.txtに書き込む。
             echo "$serviceName:$userName:$password" >> passwords.txt
 
-           # 入力ファイルを暗号化して出力ファイルに保存する
+           # 入力ファイルを暗号化して出力ファイルに保存する。
            gpg -r "$gpg_email" -e -o password.gpg password.txt
 
             echo "パスワードの追加は成功しました。"
@@ -42,17 +45,17 @@ while true; do
             ;;
         "Get Password")
             read -p "サービス名を入力してください: " serviceName
-            # 復号化したデータを一時ファイルに保存
+            # 復号化したデータを一時ファイルに保存。
             gpg -d password.gpg > password.txt 2> /dev/null
-            # serviceNameに対応するpassをpasswords.txtファイルから取得
-            # cut -d: -f3: 「:」で区切られた3番目のフィールド（パスワード）を取得
+            # serviceNameに対応するpassをpasswords.txtファイルから取得。
+            # cut -d: -f3: 「:」で区切られた3番目のフィールド（パスワード）を取得。
             password=$(grep "^$serviceName:" passwords.txt | cut -d: -f3)
 
             if [ -z "$password" ]; then
                 echo "そのサービスは登録されていません。"
             else
                 echo "サービス名：$serviceName"
-                # サービス名に対応するユーザー名を表示
+                # サービス名に対応するユーザー名を表示。
                 # cut -d: -f2: 「:」で区切られた2番目のフィールド（ユーザー名）を取得。
                 echo "ユーザー名：$(grep "^$serviceName:" passwords.txt | cut -d: -f2)"
                 # サービス名に対応するパスワードを表示。
